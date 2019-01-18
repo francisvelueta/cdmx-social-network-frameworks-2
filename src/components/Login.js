d8c1b0e66e8373a5d4155ab2511dfef8cf8ee498
francisvelueta <fvelueta@outlook.com>
2019-01-17 22:44:32 -0600

    add inputs and buttons in SignIn

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Row, Col } from 'react-materialize';
 
 import logo from './../assets/logo.jpg';
+import SignIn from './SignIn';
+
 class Login extends Component {
   render() {
     return (
@@ -14,6 +16,10 @@ class Login extends Component {
 <Col l={12} className="center-align">
 <h2>Inicia Sesión o Registrate</h2>
 </Col>
+<Col l={4} offset ="l2" className="center-align">
+<SignIn/>
+</Col>
+
 </Row>
 </Row>
 </div>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Row, Col, Input, Button } from 'react-materialize';
 
 class SignIn extends Component {
   render() {
     return(
-      <div>
-      </div>
-
+      <Row >
+      <Col l={12} >
+      <Input type="email" label="Email" validate l={12} />
+      <Input type="password" label="Password" l={12}  />
+      <Button className="amber darken-1" waves='red'>Entrar</Button>
+      </Col>
+      <Col l={4} offset="l4" className="margin-top">
+      <i  className="left fab fa-facebook-f font-ico"></i>
+      <i  className="right fab fa-google-plus-g font-ico"></i>
+      </Col>
+      </Row>
     );
     }
 }
